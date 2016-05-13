4cc3a6803298dac566eed46eca419e5c7f3603a4
x-Shen <shenx3@uci.edu>
2016-05-13 11:10:19 -0700

    start front end

---
+++ routes/index.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Forum with Voting' });
+});
+
+router.get('/signup',function(req, res, next){
+  res.render('user_related/signup_login.ejs', {title:'Sign Up', sub_title:'Please Fill the form to Sign Up'});
+});
+
+router.get('/login', function(req, res, next){
+
+});
+
+router.get('/newthread', function(req, res, next){
+
+});
+
+router.get('/editthread', function(req, res, next){
+
+});
+
+router.get('/submitthread', function(req,res,next){
+
+});
+
+router.post('/editmessage', function(req,res, next){
+
+});
+
+router.post('/upvotethread/:id', function(req,res,next){
+
+});
+
+router.post('/upvotemessage/:id', function(req, res, next){
+
+});
+
+router.post('/downvotethread/:id', function(req, res, next){
+
+});
+
+module.exports = router;
